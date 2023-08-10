3ef84c53d3b7c7c7fe70bc959f003f14ff35f910
fmagmych <mychaelmagnuson@gmail.com>
2023-08-10 15:28:24 -0500

    Adding api requests for logging in, checking in the logged in user, and registering a new user

--- src/API/index.js
+++ src/API/index.js
@@ -2,7 +2,10 @@ const cohortName = "2302-ACC-CT-WEB-PT-B";
 const API_URL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
 const POSTS_API_URL = `${API_URL}/posts`;
 const LOGIN_API_URL = `${API_URL}/users/login`;
+const LOGGEDIN_API_URL = `${API_URL}/users/me`;
+const REGISTER_API_URL = `${API_URL}/users/register`;
 
+// Fetch all posts: unauthenticated
 export async function fetchPosts() {
   try {
     const response = await fetch(POSTS_API_URL);
@@ -15,6 +18,7 @@ export async function fetchPosts() {
 
 fetchPosts();
 
+// Fetch login, authenticating a user if their username and password mathes the right data
 export async function fetchLogin(username, password) {
   try {
     const response = await fetch(LOGIN_API_URL, {
@@ -30,9 +34,50 @@ export async function fetchLogin(username, password) {
       }),
     });
     const result = await response.json();
-    // console.log(result);
+    console.log(result);
     return result;
   } catch (error) {
     console.error(error);
+  }
+}
+
+// Fetching a token based on if a user is currently logged in
+export async function fetchLoggedIn(token) {
+  try {
+    const response = await fetch(LOGGEDIN_API_URL, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Registering a new user
+export async function fetchNewUser(username, password) {
+  try {
+    const response = await fetch(REGISTER_API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: `${username}`,
+          password: `${password}`,
+        },
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
   }
 }
