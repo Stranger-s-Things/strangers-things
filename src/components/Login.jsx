de2a71dcac21aa71dbf947d8087ea693df815a0f
fmagmych <mychaelmagnuson@gmail.com>
2023-08-10 14:02:52 -0500

    Building controlled form for logging a user in, form has state on inputs and works with api fetch request

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+import { fetchLogin } from "../API/index.js";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const user = await fetchLogin(username, password);
+    if (!user.success) {
+      setError(user.error);
+    }
+    if (user.success) {
+      setSuccessMessage(user.success);
+      navigate(`/profile`);
+    }
+  }
+
   return (
     <div>
-      <p>Login</p>
+      <h1>Login</h1>
+      <form method="POST" onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button>Log in</button>
+      </form>
+
+      {error && <h3>{error.message}</h3>}
+      {successMessage && <h3>{successMessage.message}</h3>}
     </div>
   );
 }
