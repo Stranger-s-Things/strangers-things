7eb98559851c44bc59bcc3388a6e4a875c91c299
fmagmych <mychaelmagnuson@gmail.com>
2023-08-10 14:03:25 -0500

    Making sure navbar works properly and page routes work

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <header>
-      <Link to="./home">Stranger's Things </Link>
-      <Link to="./posts">Posts </Link>
-      <Link to="./login">Login</Link>
+      <h1>Stranger&apos;s Things</h1>
+      <nav>
+        <Link to="./">Home </Link>
+        <Link to="./posts">Posts </Link>
+        <Link to="./account/login">Login</Link>
+      </nav>
     </header>
   );
 }
