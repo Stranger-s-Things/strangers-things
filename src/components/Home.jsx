1c4ea358daa50dffe198fc10076b667b23199d13
fmagmych <mychaelmagnuson@gmail.com>
2023-08-10 16:02:52 -0500

    Getting the home page to display a message if the user is not currently logged in and it should work dynamically if the user is logged in

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import { useNavigate, Link } from "react-router-dom";
+
+export default function Home({ activeUser }) {
+  const navigate = useNavigate();
   return (
     <div>
-      <p>Home</p>
+      {activeUser ? (
+        <h2>Logged in as {activeUser}</h2>
+      ) : (
+        <h2>You are not logged in please log in for the best experience.</h2>
+      )}
+      <button
+        onClick={() => {
+          activeUser ? navigate("/profile") : navigate("/account/login");
+        }}
+      >
+        {activeUser ? "VIEW PROFILE" : "LOG IN"}
+      </button>
     </div>
   );
 }
