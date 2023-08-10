3f6f73920daf3357148a604c548d1c51a4a62671
fmagmych <mychaelmagnuson@gmail.com>
2023-08-10 14:02:13 -0500

    Resetting home page on layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import NavBar from "./NavBar";
-import Posts from "./Posts";
-import Login from "./Login";
-
 export default function Home() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <NavBar />
+      <p>Home</p>
     </div>
   );
 }
