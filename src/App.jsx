81649a5791ba792f913155d354a57bf0d354ab2b
fmagmych <mychaelmagnuson@gmail.com>
2023-08-10 15:29:37 -0500

    Adjusting scope of some states in order for other components to be able to access them and make the code more dry; adding states and setstates as props in some components where it will be needed later

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,42 @@ import Navbar from "./components/NavBar.jsx";
 import Posts from "./components/Posts.jsx";
 import Login from "./components/Login.jsx";
 import Profile from "./components/Profile.jsx";
+import Register from "./components/Register.jsx";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
+  const [inputType, setInputType] = useState("password");
+  const [userToken, setUserToken] = useState(null);
+  const [activeUser, setActiveUser] = useState(null);
+
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/account/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home activeUser={activeUser} />} />
+        <Route path="/posts" element={<Posts activeUser={activeUser} />} />
+        <Route
+          path="/account/login"
+          element={
+            <Login
+              inputType={inputType}
+              onSetInputType={setInputType}
+              onSetUserToken={setUserToken}
+              onSetActiveUser={setActiveUser}
+            />
+          }
+        />
+        <Route
+          path="/account/register"
+          element={
+            <Register inputType={inputType} onSetInputType={setInputType} />
+          }
+        />
+        <Route
+          path="/profile"
+          element={<Profile userToken={userToken} activeUser={activeUser} />}
+        />
       </Routes>
     </>
   );
