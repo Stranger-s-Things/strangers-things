c903223f67661585e74d4be91d1b4a0caba1edcd
fmagmych <mychaelmagnuson@gmail.com>
2023-08-10 16:36:40 -0500

    WIP: data fetch and useEffect not working 100%

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,28 @@
-export default function Profile() {
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { fetchLoggedIn } from "../API/index.js";
+
+export default function Profile({ userToken }) {
+  const [activeUser, setActiveUser] = useState(null);
+  const navigate = useNavigate();
+  if (!userToken) navigate("/account/login");
+
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const data = await fetchLoggedIn(userToken);
+        console.log(data);
+        return setActiveUser(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProfile();
+  }, [userToken]);
   return (
     <>
-      <h1>Profile</h1>
+      <h1>Welcome {activeUser.username}</h1>
+      <p></p>
     </>
   );
 }
