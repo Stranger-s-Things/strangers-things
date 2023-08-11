f832b45dbba5d6266743360aec86912e39ef33c9
fmagmych <mychaelmagnuson@gmail.com>
2023-08-11 11:08:36 -0500

    Adding a CTA message, and classname

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,27 @@
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Home({ activeUsername }) {
   const navigate = useNavigate();
+
   console.log(activeUsername);
   return (
-    <div>
-      {activeUsername ? (
-        <h2>Logged in as {activeUsername}</h2>
-      ) : (
-        <h2>You are not logged in please log in for the best experience.</h2>
-      )}
-      <button
-        onClick={() => {
-          activeUsername ? navigate("/profile") : navigate("/account/login");
-        }}
-      >
-        {activeUsername ? "VIEW PROFILE" : "LOG IN"}
-      </button>
+    <div id="home-cont">
+      <h1>Welcome to Stranger&apos;s Things</h1>
+      <h2>Anything. Anywhere.</h2>
+      <div>
+        {activeUsername ? (
+          <h3>Logged in as {activeUsername}</h3>
+        ) : (
+          <h3>You are not logged in please log in for the best experience.</h3>
+        )}
+        <button
+          onClick={() => {
+            activeUsername ? navigate("/profile") : navigate("/account/login");
+          }}
+        >
+          {activeUsername ? "VIEW PROFILE" : "LOG IN"}
+        </button>
+      </div>
     </div>
   );
 }
