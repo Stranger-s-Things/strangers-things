7a0c30bb8f10714b8fa663e8a52d03a9fa0b7e51
fmagmych <mychaelmagnuson@gmail.com>
2023-08-11 18:26:19 -0500

    Minor adjustment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,13 @@ export default function NavBar() {
     <header>
       <h1>Stranger&apos;s Things</h1>
       <nav>
-        <Link to="./" className="nav-link">
+        <Link to="/" className="nav-link">
           Home{" "}
         </Link>
-        <Link to="./posts" className="nav-link">
+        <Link to="/posts" className="nav-link">
           Posts{" "}
         </Link>
-        <Link to="./account/login" className="nav-link">
+        <Link to="/account/login" className="nav-link">
           Login
         </Link>
       </nav>
