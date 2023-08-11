87755c4e48f65c7883ba0e85388aa2ddbe01a753
fmagmych <mychaelmagnuson@gmail.com>
2023-08-11 11:07:36 -0500

    Adjusting components layout in return to help with footer placement; adding a useLocation to help with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,67 @@
-import "./App.css";
 import Home from "./components/Home.jsx";
 import Navbar from "./components/NavBar.jsx";
 import Posts from "./components/Posts.jsx";
 import Login from "./components/Login.jsx";
 import Profile from "./components/Profile.jsx";
 import Register from "./components/Register.jsx";
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer.jsx";
+
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 export default function App() {
   const [inputType, setInputType] = useState("password");
   const [userToken, setUserToken] = useState(null);
   const [activeUsername, setActiveUsername] = useState("");
 
+  const location = useLocation();
+
+  // console.log(location);
+  useEffect(() => {
+    if (location.pathname === "/") {
+      document.getElementById("main-cont").classList.add("home-page");
+    } else {
+      document.getElementById("main-cont").classList.remove("home-page");
+    }
+  }, [location.pathname]);
+
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home activeUsername={activeUsername} />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route
-          path="/account/login"
-          element={
-            <Login
-              inputType={inputType}
-              onSetInputType={setInputType}
-              onSetUserToken={setUserToken}
-            />
-          }
-        />
-        <Route
-          path="/account/register"
-          element={
-            <Register inputType={inputType} onSetInputType={setInputType} />
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <Profile
-              userToken={userToken}
-              onSetActiveUsername={setActiveUsername}
-            />
-          }
-        />
-      </Routes>
-    </>
+    <section>
+      <div id="main-cont">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home activeUsername={activeUsername} />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route
+            path="/account/login"
+            element={
+              <Login
+                inputType={inputType}
+                onSetInputType={setInputType}
+                onSetUserToken={setUserToken}
+              />
+            }
+          />
+          <Route
+            path="/account/register"
+            element={
+              <Register inputType={inputType} onSetInputType={setInputType} />
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                userToken={userToken}
+                onSetActiveUsername={setActiveUsername}
+              />
+            }
+          />
+        </Routes>
+      </div>
+      <Footer />
+    </section>
   );
 }
