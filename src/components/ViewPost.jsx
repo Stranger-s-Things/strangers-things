ad4b7dea31c5cad79ea118e6c0cfa6f327c2adf0
fmagmych <mychaelmagnuson@gmail.com>
2023-08-12 13:29:44 -0500

    Component is not built out to be functional its just a placeholder for whoever ends up doing it

---
+++ src/components/ViewPost.jsx
@@ -0,0 +1,13 @@
+// The code can probably be different but I thought maybe to make things easier we could have the message
+// poster and view post all on one component and then have it dynamically work to check if you are the poster
+// if you are arent the poster it should have the functionality to message the poster
+// IF you are the poster it should give you the option to edit or delete the post and view a list of all
+//  messages sent to you for that post
+
+export default function ViewPost() {
+  return (
+    <div>
+      <h1>View Post</h1>
+    </div>
+  );
+}
