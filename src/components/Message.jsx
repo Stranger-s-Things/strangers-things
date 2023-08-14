5e69e21b1f79beef4bbe7a57c2331bacda4fe11b
fmagmych <mychaelmagnuson@gmail.com>
2023-08-13 22:01:51 -0500

    Making sure the code is working and export default is set correctly; making sure layout works with page design

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import { postMessage } from "../API/index.js";
 
 import { useState } from "react";
 
-export default function EditPost({ userToken, sessionUserToken }) {
+export default function Message({ userToken, sessionUserToken }) {
   const [message, setMessage] = useState("");
   const [error, setError] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
@@ -13,7 +13,11 @@ export default function EditPost({ userToken, sessionUserToken }) {
     setError(null);
     setSuccessMessage(null);
 
-    const response = await postMessage(postId, sessionUserToken, message);
+    const response = await postMessage(
+      postId,
+      userToken ? userToken : sessionUserToken,
+      message
+    );
 
     console.log(response);
 
@@ -28,8 +32,6 @@ export default function EditPost({ userToken, sessionUserToken }) {
 
   return (
     <>
-      {error && <h4>{error.message}</h4>}
-      {successMessage && <h4 id="success">{successMessage}</h4>}
       <div className="form-cont">
         <h1>Message</h1>
         <form method="POST" onSubmit={handleSubmit} className="form">
@@ -46,6 +48,10 @@ export default function EditPost({ userToken, sessionUserToken }) {
             <button id="post-form-btn">Send</button>
           </div>
         </form>
+        <div>
+          {error && <h4>{error.message}</h4>}
+          {successMessage && <h4 id="success">{successMessage}</h4>}
+        </div>
       </div>
     </>
   );
