f61ba3ce69c9f32a014cf8f6d4c848878cf31085
fmagmych <mychaelmagnuson@gmail.com>
2023-08-16 09:47:04 -0500

    Adding the re-enter password input to allow for a password verification before registration form is submitted

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,17 +6,26 @@ import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 export default function Register({ inputType, onSetInputType }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
   const [successfulSignup, setSuccessfulSignup] = useState(null);
   const [error, setError] = useState(null);
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const newUser = await fetchNewUser(username, password);
-    if (newUser.error) setError(newUser.error);
-    if (newUser.success) {
-      setSuccessfulSignup(newUser.data.message);
+    setError(null);
+    setSuccessfulSignup(null);
+    if (verifyPassword === password) {
+      const newUser = await fetchNewUser(username, password);
+      if (newUser.error) setError(newUser.error);
+      if (newUser.success) {
+        setSuccessfulSignup(newUser.data.message);
+      }
+      console.log(newUser);
+    } else {
+      setError(
+        "Password inputs do not match, please make sure they match before submitting"
+      );
     }
-    console.log(newUser);
   }
 
   return (
@@ -53,7 +62,32 @@ export default function Register({ inputType, onSetInputType }) {
             />
           )}
         </div>
-
+        <div className="password-cont">
+          <input
+            className="password-input"
+            type={inputType}
+            placeholder="Re-Enter Password"
+            value={verifyPassword}
+            onChange={(e) => setVerifyPassword(e.target.value)}
+            required
+          />
+          {inputType === "password" ? (
+            <IoEyeOutline
+              onClick={() => onSetInputType("text")}
+              className="icon"
+            />
+          ) : (
+            <IoEyeOffOutline
+              onClick={() => onSetInputType("password")}
+              className="icon"
+            />
+          )}
+        </div>
+        {verifyPassword !== password ? (
+          <h3 id="password-verify">Passwords do not match</h3>
+        ) : (
+          ""
+        )}
         <div>
           <button>Create Account</button>
         </div>
@@ -68,7 +102,9 @@ export default function Register({ inputType, onSetInputType }) {
       ) : (
         ""
       )}
-      {error && <h4>Oops! Something went wrong: {error}</h4>}
+      {error && (
+        <h4 id="register-error">Oops! Something went wrong: {error}</h4>
+      )}
       {successfulSignup && (
         <h4>
           {successfulSignup}{" "}
